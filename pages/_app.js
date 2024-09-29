5f582e504d3d41eb704016b9f6af1e41c9ed83af
StarsorpsDEV <starcorps@protonmail.com>
2024-09-29 23:22:32 +0300

    analytic

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import { ThirdwebProvider } from "@thirdweb-dev/react";
 import "../styles/globals.css";
+import { Analytics } from "@vercel/analytics/react"
 
 // This is the chain your dApp will work on.
 const activeChain = "avalanche";
@@ -7,9 +8,10 @@ const activeChain = "avalanche";
 function MyApp({ Component, pageProps }) {
   return (
     <ThirdwebProvider
-      activeChain={activeChain}
-      clientId={process.env.NEXT_PUBLIC_TEMPLATE_CLIENT_ID}
+    activeChain={activeChain}
+    clientId={process.env.NEXT_PUBLIC_TEMPLATE_CLIENT_ID}
     >
+      <Analytics/>
       <Component {...pageProps} />
     </ThirdwebProvider>
   );
